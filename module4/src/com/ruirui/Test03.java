47d5165e77d0112362713a8a377407b43cacc69b
itCW883 <3491306073@qq.com>
2025-03-20 20:53:00 +0800

    Changes

--- module4/src/com/ruirui/Test03.java
+++ module4/src/com/ruirui/Test03.java
@@ -2,7 +2,7 @@ package com.ruirui;
 import recursion.RecrusionRabit;
 public class Test03 {
     public static void main(String[] args) {
-        int a=RecrusionRabit.TuZ(6);
+        int a=RecrusionRabit.TuZ(10);
         System.out.println(a);
     }
 }
